c188e540327f94db98e2884babee562c68b90fc0
Eric Janero <ejanerop97@gmail.com>
2022-03-24 00:26:37 -0400

    Created calculator structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className="calculator">
+                <div className="calculator-display"></div>
+                <div className="calculator-keys">
+                    <div className="calculator-keys-row">
+                        <button className="calculator-key key-clear key-operator">
+                            AC
+                        </button>
+                        <button className="calculator-key key-divide key-operator">
+                            /
+                        </button>
+                        <button className="calculator-key key-multiply key-operator">
+                            *
+                        </button>
+                    </div>
+                    <div className="calculator-keys-row">
+                        <button className="calculator-key key-7">7</button>
+                        <button className="calculator-key key-8">8</button>
+                        <button className="calculator-key key-9">9</button>
+                        <button className="calculator-key key-subtract key-operator">
+                            -
+                        </button>
+                    </div>
+                    <div className="calculator-keys-row">
+                        <button className="calculator-key key-4">4</button>
+                        <button className="calculator-key key-5">5</button>
+                        <button className="calculator-key key-6">6</button>
+                        <button className="calculator-key key-add key-operator">
+                            +
+                        </button>
+                    </div>
+                    <div className="calculator-keys-row">
+                        <button className="calculator-key key-1">1</button>
+                        <button className="calculator-key key-2">2</button>
+                        <button className="calculator-key key-3">3</button>
+                        <button className="calculator-key key-equals">=</button>
+                    </div>
+                    <div className="calculator-keys-row">
+                        <button className="calculator-key key-0">0</button>
+                        <button className="calculator-key key-decimal">
+                            .
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
